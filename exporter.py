6eea0129bfedc6e6df3f104a3733267beaece4a4
Dominique B <domi_burnand@yahoo.de>
2020-03-30 14:50:03 +0200

    [Change] Do not query all subsciption details at start. Index a total subscription counter

--- exporter.py
+++ exporter.py
@@ -14,6 +14,10 @@ g_active_sessions = Gauge('active_session_count', 'Number of sessions currently 
 g_registration_callees = Gauge('active_callee_count', 'Number of sessions currently attached to the registration',
                                ['router_url', 'realm', 'uri'])
 
+g_wamp_subscriptions = Gauge(
+    'number_of_subscriptions', 'Total number of subscriptions',
+    ['router_url', 'realm', 'match'])
+
 g_subscription_subscribers = Gauge('active_subscription_count',
                                    'Number of sessions currently subscribed to the subscription',
                                    ['router_url', 'realm', 'uri'])
@@ -92,20 +96,43 @@ def update_registration_callee_count(id):
 
 
 def init_subscription_subscriber_count():
-    rpc_list = yield meta['session'].call('wamp.subscription.list')
-    for id in rpc_list['exact']:
-        yield from create_subscription(id)
+    sub_list = yield meta['session'].call('wamp.subscription.list')
+
+    for match in sub_list:
+        amount = len(sub_list[match])
+        g_wamp_subscriptions.labels(
+            settings.get('wamp_url'),
+            settings.get('wamp_realm'),
+            match
+        ).set(amount)
+        print("add amount of", match, amount)
+
+    # We have thousands of subscriptions on our prod. Do not take the risk
+    # of crashing by issuing all those queries...
+    # for id in sub_list['exact']:
+    #     yield from create_subscription(id)
 
 
 def create_subscription(id):
     info = yield meta['session'].call('wamp.subscription.get', id)
-    meta['subscriptions'][id] = info['uri']
+    g_wamp_subscriptions.labels(
+        settings.get('wamp_url'),
+        settings.get('wamp_realm'),
+        info['match']
+    ).inc()
+    meta['subscriptions'][id] = {'uri': info['uri'], 'match': info['match']}
     yield from update_subscription_subscriber_count(id)
 
 
 def remove_subscription(id):
-    uri = meta['subscriptions'].get(id, None)
+    details = meta['subscriptions'].get(id, {})
+    uri = details.get('uri')
     if uri:
+        g_wamp_subscriptions.labels(
+            settings.get('wamp_url'),
+            settings.get('wamp_realm'),
+            details.get('match')
+        ).dec()
         del meta['subscriptions'][id]
         try:
             g_subscription_subscribers.remove(
@@ -118,7 +145,8 @@ def remove_subscription(id):
 
 
 def update_subscription_subscriber_count(id):
-    uri = meta['subscriptions'].get(id)
+    details = meta['subscriptions'].get(id, {})
+    uri = details.get('uri')
     if not uri:
         return  # subscription right after create, count will be updated by create
     try:
